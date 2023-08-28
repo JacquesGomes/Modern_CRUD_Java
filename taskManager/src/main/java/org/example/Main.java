7c6de94652a5350d786cc2667ddc58557cd91c0d
Jacques Gomes <113613459+JacquesGomes@users.noreply.github.com>
2023-08-28 17:45:36 -0300

    ok

--- taskManager/src/main/java/org/example/Main.java
+++ taskManager/src/main/java/org/example/Main.java
@@ -3,7 +3,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
     @Override
@@ -14,4 +16,5 @@ public class Main extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
 }
--- taskManager/src/main/java/org/example/controller/TaskController.java
+++ taskManager/src/main/java/org/example/controller/TaskController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/tasks")
+@RequestMapping("/tarefas")
 public class TaskController {
     private final TaskService taskService;
 
